c320059c26727c5d732f4aa5c771827566b1a8d0
kanish <kanishsnh@gmail.com>
2022-06-03 12:00:09 +0530

    commit

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile('index.html', {root: 'dist/frontend/'});
+    res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 8000);
